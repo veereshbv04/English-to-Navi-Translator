a3379a2820f67b349af0e34674b10ff7efe32a9b
Veeresh B V <Veereshbv04@gmail.com>
2021-07-27 15:41:01 +0530

    test:API working Working

---
+++ app.js
@@ -0,0 +1,31 @@
+console.log("Script is working from a differsnt file");
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output-div");
+
+var serverURL = "https://api.funtranslations.com/translate/navi.json" ;
+
+function getserverURL(text){
+    return serverURL + "?" + "text=" + text ;
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something wrong with server,try again");
+}
+
+function clickHandler(){
+
+    var inputText = textInput.value;
+
+    fetch(getserverURL(inputText))
+    .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+
+}
+
+btnTranslate.addEventListener("click",clickHandler);
